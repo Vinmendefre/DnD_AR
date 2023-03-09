8d799764beb97cc6b2b1745ed35fc8ec277f8d3b
Melisa <melcafidan@gmail.com>
2023-03-09 15:23:48 +0100

    Auto assign slider and fill

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -8,6 +8,13 @@ public class HealthBar : MonoBehaviour
     public Slider slider;
     public Gradient gradient;
     public Image fill;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+        fill = gameObject.transform.Find("Fill").GetComponent<Image>();
+    }
+
     public void SetMaxHealth(float health)
     {
         slider.maxValue = health;
@@ -15,10 +22,10 @@ public class HealthBar : MonoBehaviour
 
         fill.color = gradient.Evaluate(1f);
     }
+
     public void SetHealth(float health)
     {
         slider.value = health;
         fill.color = gradient.Evaluate(slider.normalizedValue);
     }
-    
 }
