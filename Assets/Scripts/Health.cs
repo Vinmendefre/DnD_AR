c0bfc676f9ec3c30613895ac72172e7b98f86271
Melisa <72318081+mleisa@users.noreply.github.com>
2023-03-09 11:55:08 +0100

    Health (#4)
    * Create Health bar
    
    * Health system in place
    
    * Health scene
    
    * Health scene
    
    * Remove heart from health bar

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] private float currentHealth;
+        [SerializeField] private float maxHealth = 100f;
+        [SerializeField] private HealthBar healthBar;
+        [SerializeField] private bool isDead = false;
+
+        public float CurrentHealth
+        {
+            get => currentHealth;
+            set => currentHealth = value;
+        }
+
+        void Start()
+        {
+            CurrentHealth = maxHealth;
+
+            if (healthBar != null)
+            {
+                healthBar.SetMaxHealth(maxHealth);
+            }
+        }
+
+        public void TakeDamage(float number)
+        {
+            if (currentHealth > 0)
+            {
+                CurrentHealth -= number;
+
+                if (healthBar != null)
+                {
+                    healthBar.SetHealth(CurrentHealth);
+                }
+            }
+
+            if (currentHealth <= 0)
+            {
+                isDead = true;
+            }
+        }
+    }
+}
